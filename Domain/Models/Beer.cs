44959e6fa9b20ba3204b116839aac5d58b47baf0
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-24 13:11:21 +0200

    Create beer model

---
+++ Domain/Models/Beer.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Models;
+public class Beer
+{
+    public string Id { get; set; } = default!;
+    public Brewer Brewer { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public string? ImageUrl { get; set; } = default;
+    public string Flavor { get; set; } = default!;
+    public string? Description { get; set; } = default;
+    public decimal Price { get; set; } = default;
+}
