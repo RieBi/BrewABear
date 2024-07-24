0b5041265f4160ec4049fb17a992c29031b1d5d5
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-24 16:23:46 +0200

    Add data seeding for beers

--- Data/DataSeeding/DataSeeder.cs
+++ Data/DataSeeding/DataSeeder.cs
@@ -50,6 +50,46 @@ public class DataSeeder(DataContext context)
 
         _context.Brewers.AddRange(brewers);
 
+        IList<Beer> beers =
+        [
+            new()
+            {
+                Id = CreateGuid(),
+                Brewer = brewers[0],
+                Name = "Quaternibeer",
+                Description = "Juicy beer, made for you.",
+                Flavor = "Moustache grape",
+                Price = 100,
+            },
+            new()
+            {
+                Id = CreateGuid(),
+                Brewer = brewers[0],
+                Name = "Bolsoyam",
+                Description = "Bean beer for Mr. Bean.",
+                Flavor = "Quirky apathy",
+                Price = 77,
+            },
+            new()
+            {
+                Id = CreateGuid(),
+                Brewer = brewers[1],
+                Name = "Big beer",
+                Description = "All you need for a day",
+                Flavor = "Strawberries",
+                Price = 222,
+            },
+            new()
+            {
+                Id = CreateGuid(),
+                Brewer = brewers[1],
+                Name = "PyPy",
+                Description = "Bites like a snake, feels like honey.",
+                Flavor = "Slow",
+                Price = 50,
+            }
+        ];
+
         _context.SaveChanges();
     }
 
--- Domain/Models/Beer.cs
+++ Domain/Models/Beer.cs
@@ -4,7 +4,6 @@ public class Beer
     public string Id { get; set; } = default!;
     public Brewer Brewer { get; set; } = default!;
     public string Name { get; set; } = default!;
-    public string? ImageUrl { get; set; } = default;
     public string Flavor { get; set; } = default!;
     public string? Description { get; set; } = default;
     public decimal Price { get; set; } = default;
