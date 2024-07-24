b0ac4ff6abd3e4721f6fdb43919710a8eecb3470
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-24 13:43:16 +0200

    Create Data project

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data;
-public class DataContext
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
 {
+    public DbSet<Beer> Beers { get; set; }
+    public DbSet<BeerSale> BeerSales { get; set; }
+    public DbSet<Brewer> Brewers { get; set; }
+    public DbSet<Brewery> Breweries { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Wholesaler> Wholesalers { get; set; }
+    public DbSet<WholesalerInventory> WholesalerInventories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
 }
