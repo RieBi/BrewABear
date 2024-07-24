aae5923be180f2caefb45f6a0e41d5da3d71b90e
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-24 15:54:59 +0200

    Add configuration for Wholesaler

---
+++ Data/ModelConfigurations/WholesalerConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.ModelConfigurations;
+internal class WholesalerConfiguration : IEntityTypeConfiguration<Wholesaler>
+{
+    public void Configure(EntityTypeBuilder<Wholesaler> builder)
+    {
+        builder.HasKey(f => f.Id);
+
+        builder.Property(f => f.Name).HasMaxLength(DomainConfig.NameLength);
+
+        builder
+            .HasMany(f => f.InventoryItems)
+            .WithOne(f => f.Wholesaler);
+    }
+}
--- Domain/Models/Wholesaler.cs
+++ Domain/Models/Wholesaler.cs
@@ -3,4 +3,6 @@ public class Wholesaler
 {
     public string Id { get; set; } = default!;
     public string Name { get; set; } = default!;
+
+    public IList<WholesalerInventory> InventoryItems { get; set; } = [];
 }
