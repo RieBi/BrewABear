0e7e76c713761c15b275e5bbb0839653f444b696
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-24 16:18:54 +0200

    Add seeding for brewers

--- Data/DataSeeding/DataSeeder.cs
+++ Data/DataSeeding/DataSeeder.cs
@@ -1,4 +1,6 @@
-﻿namespace Data.DataSeeding;
+﻿using Domain.Models;
+
+namespace Data.DataSeeding;
 public class DataSeeder(DataContext context)
 {
     private readonly DataContext _context = context;
@@ -8,23 +10,48 @@ public class DataSeeder(DataContext context)
         _context.Database.EnsureDeleted();
         _context.Database.EnsureCreated();
 
-        _context.Breweries.AddRange(
-            [
-                new()
-                {
-                    Id = Guid.NewGuid().ToString(),
-                    Name = "Bear Brewery",
-                    Address = "Prague, Big Street, 752/13",
-                },
-                new()
-                {
-                    Id = Guid.NewGuid().ToString(),
-                    Name = "Alien Hot Brewery",
-                    Address = "Mars, Small Crater, Loophole 17, 1",
-                }
-            ]
-        );
+        IList<Brewery> breweries =
+        [
+            new()
+            {
+                Id = CreateGuid(),
+                Name = "Bear Brewery",
+                Address = "Prague, Big Street, 752/13",
+            },
+            new()
+            {
+                Id = CreateGuid(),
+                Name = "Alien Hot Brewery",
+                Address = "Mars, Small Crater, Loophole 17, 1",
+            }
+        ];
+
+        _context.Breweries.AddRange(breweries);
+
+        IList<Brewer> brewers =
+        [
+            new()
+            {
+                Id = CreateGuid(),
+                Brewery = breweries[0],
+                FirstName = "John",
+                LastName = "Wick",
+                ContactEmail = "john.superwick@superjohnwick.example.org"
+            },
+            new()
+            {
+                Id = CreateGuid(),
+                Brewery = breweries[1],
+                FirstName = "Pablo",
+                LastName = "Biden",
+                ContactEmail = "pablo1234qwerty@biden.example.org"
+            },
+        ];
+
+        _context.Brewers.AddRange(brewers);
 
         _context.SaveChanges();
     }
+
+    private string CreateGuid() => Guid.NewGuid().ToString();
 }
