c890de94e63d8cd30391f28467399fd484b4a55f
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-24 18:04:04 +0200

    Create initial controller

---
+++ Api/Controllers/BreweryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class BreweryController : ControllerBase
+{
+    [HttpGet]
+    [Route("All")]
+    [Route("")]
+    public string All()
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
