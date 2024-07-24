e1842df876af244f56df6e5e683286f5b21fe278
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-24 15:52:48 +0200

    Add configuration for Order

---
+++ Data/ModelConfigurations/OrderConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.ModelConfigurations;
+internal class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(f => f.Id);
+
+        builder.Property(f => f.ClientEmail).HasMaxLength(DomainConfig.NameLength);
+
+        builder.HasOne(f => f.Beer);
+        builder.HasOne(f => f.Wholesaler);
+    }
+}
