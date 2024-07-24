1c9ad85fec2b312f6f2e26a251a39d2056628a12
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-24 13:19:19 +0200

    Add wholesalerInventory model

---
+++ Domain/Models/WholesalerInventory.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Models;
+public class WholesalerInventory
+{
+    public Wholesaler Wholesaler { get; set; } = default!;
+    public Beer Beer { get; set; } = default!;
+    public int Quantity { get; set; }
+}
