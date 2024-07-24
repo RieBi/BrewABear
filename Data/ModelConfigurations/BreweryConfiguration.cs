c7e3f27fbfd2613bb38ca5af23b4c9e25f6142db
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-24 15:41:49 +0200

    Add configuration for brewery

---
+++ Data/ModelConfigurations/BreweryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.ModelConfigurations;
+internal class BreweryConfiguration : IEntityTypeConfiguration<Brewery>
+{
+    public void Configure(EntityTypeBuilder<Brewery> builder)
+    {
+        builder.HasKey(f => f.Id);
+        builder.Property(f => f.Name).HasMaxLength(DomainConfig.NameLength);
+        builder.Property(f => f.Address).HasMaxLength(200);
+    }
+}
