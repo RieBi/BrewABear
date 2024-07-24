06f91a08923c0bd15b906759823cc8452b1b48eb
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-24 15:47:06 +0200

    Add configuration for BeerSale

---
+++ Data/ModelConfigurations/BeerSaleConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.ModelConfigurations;
+internal class BeerSaleConfiguration : IEntityTypeConfiguration<BeerSale>
+{
+    public void Configure(EntityTypeBuilder<BeerSale> builder)
+    {
+        builder.HasKey(f => f.Id);
+
+        builder.HasOne(f => f.Beer);
+        builder.HasOne(f => f.Wholesaler);
+    }
+}
