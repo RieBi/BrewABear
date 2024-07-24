540a6adb268b85c92bd03bc75d9cd11c449a5416
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-24 13:17:03 +0200

    Add order model

---
+++ Domain/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Models;
+public class Order
+{
+    public string Id { get; set; } = default!;
+    public string ClientEmail { get; set; } = default!;
+    public Beer Beer { get; set; } = default!;
+    public Wholesaler Wholesaler { get; set; } = default!;
+    public int Quantity { get; set; }
+    public decimal PricePerBear { get; set; }
+    public decimal DiscountPercentage { get; set; }
+}
