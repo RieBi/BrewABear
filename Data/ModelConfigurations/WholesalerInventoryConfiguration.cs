7f2558cb3549e64961f34144eb8b6414bea49873
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-24 15:57:51 +0200

    Add configuration for WholesalerInventory

---
+++ Data/ModelConfigurations/WholesalerInventoryConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.ModelConfigurations;
+internal class WholesalerInventoryConfiguration : IEntityTypeConfiguration<WholesalerInventory>
+{
+    public void Configure(EntityTypeBuilder<WholesalerInventory> builder)
+    {
+        builder.HasKey(f => new object[] { f.Beer, f.Wholesaler });
+
+        builder
+            .HasOne(f => f.Wholesaler)
+            .WithMany(f => f.InventoryItems);
+
+        builder.HasOne(f => f.Beer);
+    }
+}
