658c36a0ddec6f1719b98ffb4e4972364f1d2c1a
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-24 13:55:53 +0200

    Add connection for database

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,5 @@
+using Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new InvalidOperationException("Connection string could not be loaded from settings.");
+}
+
+builder.Services.AddDatabase(connectionString!);
 
 var app = builder.Build();
 
---
+++ Data/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Data;
+public static class DependencyInjection
+{
+    public static void AddDatabase(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<DataContext>(options =>
+        {
+            options.UseSqlite(connectionString);
+        });
+    }
+}
