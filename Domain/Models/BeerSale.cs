28e668ad3d46d9fe680798dbfdb9f898933e1e3e
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-24 13:13:27 +0200

    Add beersale model

---
+++ Domain/Models/BeerSale.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Models;
+public class BeerSale
+{
+    public string Id { get; set; } = default!;
+    public Beer Beer { get; set; } = default!;
+    public Wholesaler Wholesaler { get; set; } = default!;
+    public int Quantity { get; set; }
+}
