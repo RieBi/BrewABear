badcbe63b4db3464804b5225da71d3a279f0c360
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-24 13:07:14 +0200

    Add brewer model

---
+++ Domain/Models/Brewer.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Models;
+public class Brewer
+{
+    public string Id { get; set; } = default!;
+    public Brewery Brewery { get; set; } = default!;
+    public string FirstName { get; set; } = default!;
+    public string? LastName { get; set; } = default;
+    public string ContactEmail { get; set; } = default!;
+}
