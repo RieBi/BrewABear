cf7f09079fce1fa143caa7900fce952378879376
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-25 11:35:43 +0200

    Create ErrorDto for displaying errors

---
+++ Application/Exceptions/ErrorDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Exceptions;
+public class ErrorDto(Exception exception, string message)
+{
+    public string Exception { get; set; } = exception.GetType().Name;
+    public string Message { get; set; } = message;
+}
