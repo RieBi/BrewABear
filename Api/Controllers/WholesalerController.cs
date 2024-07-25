6a806497e56e935323d6866025ee3d6716e62c01
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-25 15:21:04 +0200

    Create Wholesaler/All action

---
+++ Api/Controllers/WholesalerController.cs
@@ -0,0 +1,22 @@
+﻿using Application.DTOs;
+using Application.Queries.WholesalerQueries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class WholesalerController(IMediator mediator) : ControllerBase
+{
+    private readonly IMediator _mediator = mediator;
+
+    [HttpGet]
+    [Route("All")]
+    [ProducesResponseType<IList<WholesalerDto>>(200)]
+    public async Task<ActionResult<IList<WholesalerDto>>> All()
+    {
+        var wholesalers = await _mediator.Send(new GetAllWholesalersQuery());
+
+        return Ok(wholesalers);
+    }
+}
