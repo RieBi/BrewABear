8dba9621f101d27e15cff2f17c3c59bf170acc28
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-25 11:40:21 +0200

    Add exceptions for 404 NotFound

---
+++ Application/Exceptions/BeerNotFoundException.cs
@@ -0,0 +1,2 @@
+﻿namespace Application.Exceptions;
+public class BeerNotFoundException(string beerId) : ResourceNotFoundException(beerId);
---
+++ Application/Exceptions/ResourceNotFoundException.cs
@@ -0,0 +1,5 @@
+﻿namespace Application.Exceptions;
+public class ResourceNotFoundException(string resourceId) : Exception
+{
+    public string ResourceId { get; set; } = resourceId;
+}
