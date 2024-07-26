afe38fb101e9aca7cf38b1c76b5952cc25d50883
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-26 07:35:18 +0200

    Create OrderService

---
+++ Application/Services/IOrderService.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Application.Services;
+
+public interface IOrderService
+{
+    decimal GetFinalPrice(Order order);
+    decimal GetQuotaPercentage(Order order);
+}
---
+++ Application/Services/OrderService.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Services;
+public class OrderService : IOrderService
+{
+    public decimal GetFinalPrice(Order order) => order.Quantity * order.PricePerBear;
+
+    public decimal GetQuotaPercentage(Order order)
+    {
+        return order.Quantity switch
+        {
+            > 20 => .2M,
+            > 10 => .1M,
+            _ => 0
+        };
+    }
+}
