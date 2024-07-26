eb9742ae4d9958b7b3c960a99e15382fe696edd6
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-26 07:25:01 +0200

    Add exceptions for negative numbers

---
+++ Application/Exceptions/NegativePriceException.cs
@@ -0,0 +1,5 @@
+﻿namespace Application.Exceptions;
+public class NegativePriceException(decimal price) : Exception
+{
+    public decimal Price { get; set; } = price;
+}
---
+++ Application/Exceptions/NegativeQuantityException.cs
@@ -0,0 +1,5 @@
+﻿namespace Application.Exceptions;
+public class NegativeQuantityException(int quantity) : Exception
+{
+    public int Quantity { get; set; } = quantity;
+}
