46688ba2a894b0e19543deabfef8205ba517932c
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-26 18:08:59 +0200

    Create ApiFactory

---
+++ Tests/ApiIntegrationTests/ApiFactory.cs
@@ -0,0 +1,30 @@
+﻿using Data;
+using Data.DataSeeding;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Tests.ApiIntegrationTests;
+public class ApiFactory<T> : WebApplicationFactory<T> where T : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll<DataContext>();
+
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseInMemoryDatabase(Guid.NewGuid().ToString());
+            });
+
+            var provider = services.BuildServiceProvider();
+
+            using var scope = provider.CreateScope();
+            var seeder = scope.ServiceProvider.GetRequiredService<DataSeeder>();
+            seeder.ApplySeeding();
+        });
+    }
+}
