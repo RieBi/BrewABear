75472d815c4b055c184db7ea387766152c4bd057
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-26 12:56:53 +0200

    Create tests for GuidCreator

---
+++ Tests/Application/Services/GuidCreatorTests.cs
@@ -0,0 +1,27 @@
+﻿using Application.Services;
+
+namespace Tests.Application.Services;
+public class GuidCreatorTests
+{
+    [Fact]
+    public void CreatesGuid()
+    {
+        var service = new GuidCreator();
+
+        var guid = service.Create();
+
+        Assert.NotNull(guid);
+        Assert.True(guid.Length > 10);
+    }
+
+    [Fact]
+    public void CreatesDifferentGuids()
+    {
+        var service = new GuidCreator();
+
+        var guid = service.Create();
+        var guid2 = service.Create();
+
+        Assert.NotEqual(guid, guid2);
+    }
+}
