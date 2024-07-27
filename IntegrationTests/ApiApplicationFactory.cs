931f3deefdfdf25aed45bc876687f8e900449fb4
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-27 09:34:40 +0200

    Add ApiApplicationFactory

---
+++ IntegrationTests/ApiApplicationFactory.cs
@@ -0,0 +1,26 @@
+﻿using Data;
+using Data.DataSeeding;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace IntegrationTests;
+public class ApiApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll<DataContext>();
+
+            services.AddDatabase("DataSource=:memory:");
+
+            var sp = services.BuildServiceProvider();
+
+            using var scope = sp.CreateScope();
+            var seeder = scope.ServiceProvider.GetRequiredService<DataSeeder>();
+            seeder.ApplySeeding();
+        });
+    }
+}
