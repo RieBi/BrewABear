2294789ac16c9774f4d1a55929f95e84d4503d30
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-28 12:31:27 +0200

    Create integration tests for getting orders

---
+++ IntegrationTests/ControllerTests/OrderControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using Application.DTOs;
+using FluentAssertions;
+using System.Net.Http.Json;
+
+namespace IntegrationTests.ControllerTests;
+public class OrderControllerTests(ApiApplicationFactory factory) : IClassFixture<ApiApplicationFactory>
+{
+    private readonly ApiApplicationFactory _factory = factory;
+
+    [Theory]
+    [InlineData("/Api/Order/All", "/Api/Order/{0}/Details")]
+    public async Task GetOrderEndpoints_ReturnOrders(string url1, string url2)
+    {
+        var client = _factory.CreateClient();
+
+        var orders = await client.GetFromJsonAsync<List<OrderDto>>(url1);
+
+        orders.Should().NotBeNullOrEmpty();
+        var order = orders?[0];
+        order.Should().NotBeNull();
+        order?.Id.Should().NotBeNullOrWhiteSpace();
+        order?.ClientEmail.Should().NotBeNullOrWhiteSpace();
+
+        var orderDetailed = await client.GetFromJsonAsync<OrderDto>(string.Format(url2, order?.Id));
+
+        orderDetailed.Should().NotBeNull();
+        orderDetailed.Should().BeEquivalentTo(order);
+    }
+}
